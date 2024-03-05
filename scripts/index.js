64f77238499668b28c9ba3bad178b4bce85acdf1
Franco Galdame <fracki1010@gmail.com>
2024-03-04 23:30:57 -0300

    implementacion de info

---
+++ scripts/index.js
@@ -0,0 +1,59 @@
+Vue.createApp({
+    data() {
+        return {
+            clientInfo: [],
+            accountInfo: {},
+            errorToats: null,
+            errorMsg: null,
+        }
+    },
+    methods: {
+        getDataClient: function () {
+            axios.get("/api/clients/current")
+                .then((response) => {
+                    //obtiene los datos del cliente actual y autenticado
+                    this.clientInfo = response.data;
+                })
+                .catch((error) => {
+                    // Por si no hay un cliente autenticado y no obtiene nada
+                    this.errorMsg = "Error getting data";
+                    this.errorToats.show();
+                })
+        },
+        getData: function () {
+            const urlParams = new URLSearchParams(window.location.search);
+            const id = urlParams.get('id');
+            axios.get(`/api/accounts/${id}`)
+                .then((response) => {
+                    //get client ifo
+                    this.accountInfo = response.data;
+                    this.accountInfo.transactions.sort((a, b) => (b.id - a.id))
+                })
+                .catch((error) => {
+                    // handle error
+                    this.errorMsg = "Error getting data";
+                    this.errorToats.show();
+                })
+        },
+        formatDate: function (date) {
+            return new Date(date).toLocaleDateString('en-gb');
+        },
+        signOut: function () {
+            axios.post('/api/logout')
+                .then(response => window.location.href = "/web/index.html")
+                .catch(() => {
+                    this.errorMsg = "Sign out failed"
+                    this.errorToats.show();
+                })
+        },
+        seguidoresYPublico: function () {
+            window.location.href = '../seguidoresYPublico.html';
+        },
+        
+    },
+    mounted: function () {
+        /* this.errorToats = new bootstrap.Toast(document.getElementById('danger-toast'));
+        this.getData();
+        this.getDataClient(); */
+    }
+}).mount('#app')
